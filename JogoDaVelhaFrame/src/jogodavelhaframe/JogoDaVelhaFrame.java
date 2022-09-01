b5b8664f929996eb15ca6d2cf5ae42505afd7a09
Erick Dutra <erickdutra140@gmail.com>
2022-09-01 12:12:36 -0300

    JogoDaVelha
    jogo da velha em jframe usando java
    projeto para faculdade

---
+++ JogoDaVelhaFrame/src/jogodavelhaframe/JogoDaVelhaFrame.java
@@ -0,0 +1,11 @@
+
+package jogodavelhaframe;
+public class JogoDaVelhaFrame {
+    public static void main(String[] args) {
+        
+        Principal principal = new Principal();
+        principal.setVisible(true);
+        
+    }
+    
+}
